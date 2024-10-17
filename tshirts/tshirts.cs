aea9308056e241d7a1a17f143e898e015606575c
Praveenkumar210995 <pravin21995@gmail.com>
2024-10-17 22:18:09 +0530

    Update tshirts.cs

--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -1,23 +1,22 @@
 using System;
 using System.Diagnostics;
 
-namespace TshirtSpace {
-    class Tshirt {
-        static string Size(int cms) {
-            if(cms < 38) {
+namespace TshirtSpace 
+{
+    class Tshirt
+    {
+        static string Size(int cms)
+        {
+            if(cms < 38)
+            {
                 return "S";
-            } else if(cms > 38 && cms < 42) {
+            } else if(cms > 38 && cms < 42) 
+            {
                 return "M";
-            } else {
+            } else
+            {
                 return "L";
             }
-        }
-        static void Main(string[] args) {
-            Debug.Assert(Size(37) == "S");
-            Debug.Assert(Size(40) == "M");
-            Debug.Assert(Size(43) == "L");
-            Debug.Assert(Size(38) == "S");
-            Console.WriteLine( "All is well (maybe!)");
         }
     }
 }
