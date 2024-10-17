9925ada4631c7fc0ed8475f011d58b3cfa4b3a7f
Praveenkumar210995 <pravin21995@gmail.com>
2024-10-17 22:42:29 +0530

    Update TshirtsTestCases.cs

--- tshirts/TshirtsTestCases.cs
+++ tshirts/TshirtsTestCases.cs
@@ -1,15 +1,21 @@
+
 using System;
 using System.Diagnostics;
+namespace TshirtSpace
 
-namespace TshirtSpace {
-    class Program {
-               
-        static void Main(string[] args) {
+{
+    class TestCase
+    {
+        Tshirt tshirt = new Tshirt();
+
+        static void Main(string[] args)
+        {
             Debug.Assert(Tshirt.Size(37) == "S");
             Debug.Assert(Tshirt.Size(40) == "M");
             Debug.Assert(Tshirt.Size(43) == "L");
             Debug.Assert(Tshirt.Size(38) == "M");
-            Console.WriteLine( "All is well (maybe!)");
+            Console.WriteLine("All is well (maybe!)");
         }
     }
 }
+
