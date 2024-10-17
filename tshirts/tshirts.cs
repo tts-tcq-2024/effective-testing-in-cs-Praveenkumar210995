f4861c20210dd87155bab6b08aa08b82b45fb2b7
Praveenkumar210995 <pravin21995@gmail.com>
2024-10-17 22:28:38 +0530

    Update tshirts.cs

--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -1,22 +1,17 @@
 using System;
-using System.Diagnostics;
 
-namespace TshirtSpace 
-{
-    class Tshirt
-    {
-        static string Size(int cms)
-        {
-            if(cms < 38)
-            {
+namespace TshirtSpace {
+    public class Tshirt {
+        public static string Size(int cms) {
+            if (cms < 38) {
                 return "S";
-            } else if(cms > 38 && cms < 42) 
-            {
+            } else if (cms > 38 && cms < 42) {
                 return "M";
-            } else
-            {
+            } else {
                 return "L";
             }
+
+            
         }
     }
 }
