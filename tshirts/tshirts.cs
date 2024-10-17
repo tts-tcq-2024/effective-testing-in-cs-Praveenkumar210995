a5614ba0ac9cf8d16a08b0e4b5bc8ded6b884827
Praveenkumar210995 <pravin21995@gmail.com>
2024-10-17 22:34:35 +0530

    Update tshirts.cs

--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -1,5 +1,4 @@
 using System;
-using Xunit;
 namespace TshirtSpace {
     public class Tshirt {
         public static string Size(int cms) {
@@ -12,39 +11,5 @@ namespace TshirtSpace {
             }
             
         }
-
-        public class TshirtTestCases
-{  
-    [Fact]
-    public void InputLessThan_38_ShouldReturn_S_Size() 
-    {
-        int input = 37;
-        string result = Tshirt.Size(input);
-        Assert.Equal("S", result);
-    }
-
-    [Fact]
-    public void InputBetween_38and40_ShouldReturn_M_Size() 
-    {
-        int input = 40;
-        string result = Tshirt.Size(input);
-        Assert.Equal("M", result);
-    }
-
-    [Fact]
-    public void InputGreaterThan_42_ShouldReturn_L_Size() 
-    {
-        int input = 43;
-        string result = Tshirt.Size(input);
-        Assert.Equal("L", result);
-    }
-
-    [Fact]
-    public void HandleEdgeCaseofSize_38() 
-    {
-        int input = 38;
-        string result = Tshirt.Size(input);
-        Assert.Equal("M", result);
-    }
     }
 }
