cd34c109fa2ce2b654eecc5e34a589962941a246
Praveenkumar210995 <pravin21995@gmail.com>
2024-10-17 22:33:12 +0530

    Update TshirtsTestCases.cs

--- tshirts/TshirtsTestCases.cs
+++ tshirts/TshirtsTestCases.cs
@@ -1,37 +1,15 @@
-using Xunit;
-using TshirtSpace;
+using System;
+using System.Diagnostics;
 
-public class TshirtTestCases
-{  
-    [Fact]
-    public void InputLessThan_38_ShouldReturn_S_Size() 
-    {
-        int input = 37;
-        string result = Tshirt.Size(input);
-        Assert.Equal("S", result);
-    }
-
-    [Fact]
-    public void InputBetween_38and40_ShouldReturn_M_Size() 
-    {
-        int input = 40;
-        string result = Tshirt.Size(input);
-        Assert.Equal("M", result);
-    }
-
-    [Fact]
-    public void InputGreaterThan_42_ShouldReturn_L_Size() 
-    {
-        int input = 43;
-        string result = Tshirt.Size(input);
-        Assert.Equal("L", result);
-    }
-
-    [Fact]
-    public void HandleEdgeCaseofSize_38() 
-    {
-        int input = 38;
-        string result = Tshirt.Size(input);
-        Assert.Equal("M", result);
+namespace TshirtSpace {
+    class Program {
+               
+        static void Main(string[] args) {
+            Debug.Assert(Tshirt.Size(37) == "S");
+            Debug.Assert(Tshirt.Size(40) == "M");
+            Debug.Assert(Tshirt.Size(43) == "L");
+            Debug.Assert(Tshirt.Size(38) == "M");
+            Console.WriteLine( "All is well (maybe!)");
+        }
     }
 }
